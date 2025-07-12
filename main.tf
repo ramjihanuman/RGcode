ff40c3a8ec6639b3370d1190c3727d5346303b22
gauravshukla2548 <gaurav.shukla2548@gmail.com>
2025-07-12 18:25:21 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "e8f942cf-6360-46ff-b017-31ede6559daf"
+}
+
+
+resource "azurerm_resource_group" "rg_todo" {
+    name     = "Jitu"
+    location = "East Us"
+}
+
+
+
+
+
+
+
+
